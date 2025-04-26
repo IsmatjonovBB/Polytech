11a780678738b138c5a07cefd8b8de72b842d313
Bakhtovar Ismatjonov <ismatjonovB@outlook.com>
2025-04-26 11:16:14 +0500

    Add files via upload
    Lesson of Basic Programming in Polytech | FDT | I207

---
+++ Basic Programming/app.py
@@ -0,0 +1,22 @@
+from tokenize import Number
+
+name = input("Input your name: ")
+print("Hello " + name)
+age = int(input("Input your age: "))
+
+print(2 + 3)
+print("Hello World!")
+
+# print(2 + 3)
+#     print("Hello World!") It'll be Error!!!
+
+# Print("Hello World!") Error!!!
+
+if 1 < 2:
+    print(True)
+
+print("Full name:", "Tom", "Smith")
+
+print("Hello, my name is", end= ": ")
+print(name, end= ". And ")
+print("I`m", age, "years old!")
